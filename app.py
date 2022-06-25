f21c4dd8c5b576b2de98dce4663d87eecb1cfb4f
TakayukiKomada <Takakoma.79@gmail.com>
2022-06-25 15:55:41 +0900

    超便利キー

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import requests
 
-response = requests.get('https://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111')
-print(response)
-print(response.text)
+postal_code = "0287111"
+
+response = requests.get(
+    f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}"
+)
+
+dic = response.json()
+pref_name = dic['results'][0]['address1']
+city_name = dic['results'][0]['address2']
+town_name = dic['results'][0]['address3']
+print(pref_name+city_name+town_name)
